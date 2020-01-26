5626999695f0611f951d8568736684cb8b83954d
Serina Tsang <serinat@uw.edu>
2020-01-25 16:36:09 -0800

    server.js updated

--- Develop/server.js
+++ Develop/server.js
@@ -1,31 +1,29 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useFindAndModify: false
+ });
 
-db.Workout.create({ name: "" })
-  .then(dbWorkout => {
-    console.log(dbWorkout);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
+app.use(require("./routes/api-routes.js"));
+app.use(require("./rotes/html-routes.js"));
 
-app.get("/workout", (req, res) => {
+
+ app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+/*app.get("/workout", (req, res) => {
   db.Workout.find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -44,27 +42,7 @@ app.get("/stats", (req, res) => {
       res.json(err);
     });
 });
+*/
 
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
 
-app.get("/populateduser", (req, res) => {
-  // TODO
-  // =====
-  // Write the query to grab the documents from the User collection,
-  // and populate them with any associated Notes.
-  // TIP: Check the models out to see how the Notes refers to the User
-});
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
