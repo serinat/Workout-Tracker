4453db3d5a1a696d247fa837ad041573d840363d
Serina Tsang <serinat@uw.edu>
2020-02-11 09:06:04 -0800

    updates

--- public/api.js
+++ public/api.js
@@ -1,39 +1,39 @@
 const API = {
-    async getLastWorkout() {
-      const res = await fetch("/api/workouts");
-      const json = await res.json();
-  
-      return json[json.length - 1];
-    },
-    async addExercise(data) {
-      const id = location.search.split("=")[1];
-  
+  async getLastWorkout() {
+    const res = await fetch("/api/workouts");
+    const json = await res.json();
+
+    return json[json.length - 1];
+  },
+  async addExercise(data) {
+    const id = location.search.split("=")[1];
+    if (id) {
       const res = await fetch("/api/workouts/" + id, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
       });
-  
+
       const json = await res.json();
-  
+
       return json;
-    },
-    async createWorkout() {
+    } else {
       const res = await fetch("/api/workouts", {
         method: "POST",
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
       });
-  
+
       const json = await res.json();
-  
+
       return json;
-    },
-  
-    async getWorkoutsInRange() {
-      const res = await fetch(`/api/workouts/range`);
-      const json = await res.json();
-      
-      return json;
-    },
-  };
-  
+    }
+  },
+
+  async getWorkoutsInRange() {
+    const res = await fetch(`/api/workouts/range`);
+    const json = await res.json();
+
+    return json;
+  },
+};
